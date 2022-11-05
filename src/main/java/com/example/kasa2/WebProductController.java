7da23bd80bdb6c06a4951990d5d0cf6d306037c6
devszark <devszark@gmail.com>
2022-11-05 20:14:23 +0100

    Added controller for POST method of web form (to add new products from a web page)

--- src/main/java/com/example/kasa2/WebProductController.java
+++ src/main/java/com/example/kasa2/WebProductController.java
@@ -2,7 +2,12 @@ package com.example.kasa2;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class WebProductController {
@@ -15,12 +20,14 @@ public class WebProductController {
         return "webAddProduct";
     }
 
-// Not finished - it doesn't work yet
-//    @PostMapping("/web/products/new")
-//    public int newProductPOST(@RequestBody Product newProduct) {
-//        for (Product newProduct : newProductsList){
-//            productRepository.addProduct(newProduct);
-//        }
-//        return 1;
+    @PostMapping("/web/products/new")
+    public String submit(@ModelAttribute("product")Product product,
+                         BindingResult result, ModelMap model) {
+        if (result.hasErrors()) {
+            return "error";
+        }
+        productRepository.addProduct(product);
+        return "webAddProduct";
+    }
 
 }
